2c6e7ac3d7430f4514599091a3dd620a80bf0e60
Tech Hax <38444224+TechHax@users.noreply.github.com>
2018-05-06 00:25:48 -0500

    Extension Files v1.0

---
+++ background.js
@@ -0,0 +1,14 @@
+    var host = "https://mobile.twitter.com/";
+    chrome.webRequest.onBeforeRequest.addListener(
+        function(details) {
+             return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+        },
+        {
+            urls: [
+                "*://twitter.com/*",
+                "*://www.twitter.com/*"
+            ],
+            types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+        },
+        ["blocking"]
+    );
